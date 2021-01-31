19547dfc04f8dd99363e80817089043e00510275
Paul_Mirel <paul.mirel@startmail.com>
2021-01-31 13:43:55 -0500

    include clock battery status in battery voltage test code

--- test-codes/read-battery-voltage/code.py
+++ test-codes/read-battery-voltage/code.py
@@ -1,8 +1,13 @@
 import board
 import time
+import busio
+import adafruit_pcf8523
 from analogio import AnalogIn
 
 vbat_voltage_pin = AnalogIn(board.VOLTAGE_MONITOR)
+
+I2C_bus = busio.I2C(board.SCL, board.SDA)
+rtc = adafruit_pcf8523.PCF8523(I2C_bus)
 
 
 def get_voltage( pin ):
@@ -12,8 +17,10 @@ while True:
     battery_voltage = round( get_voltage( vbat_voltage_pin ), 2 )
 
     #check clock battery OK
-    clock_battery_status = "NA"
-
+    if ( rtc.battery_low ):
+        clock_battery_status = "LOW"
+    else:
+        clock_battery_status = "OK"
     print( "Main Battery voltage: {}   Clock Battery status: {}".format( battery_voltage, clock_battery_status ) )
 
     time.sleep( 1 )
