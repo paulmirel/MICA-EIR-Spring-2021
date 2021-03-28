0527cc3efd204f2c8e6615ac162a3887b7126e4f
Alan Grover <awgrover@gmail.com>
2021-03-28 13:44:39 -0400

    proximity to neo works

--- test-codes/proximity-to-neopixel/code.py
+++ test-codes/proximity-to-neopixel/code.py
@@ -3,22 +3,22 @@ Demonstrate sensor to indicator.
 
 In this case, read proximity sensor and show as hue on neopixel.
 """
-import time
 
-# Setup I2C 
-from board import SCL, SDA
+import board
 import busio
-i2c = busio.I2C(SCL, SDA) # a ValueError typically means something got leftover. do a reset.
+from adafruit_apds9960.apds9960 import APDS9960
+import neopixel
+import adafruit_fancyled.adafruit_fancyled as fancy
+
+# Setup I2C (apds9960 is an i2c device)
+i2c = busio.I2C(board.SCL, board.SDA) # a ValueError typically means something got leftover. do a reset.
 print("I2C setup")
 
 # Setup for proximity sensor
-from adafruit_apds9960.apds9960 import APDS9960
-
-apds_first_fail = True
 
 def wait_for_apds():
     # can be adapted to wait for any I2C device
-    global apds_first_fail # suggests this should be a little class
+    apds_first_fail = True
     device = None
     while not device:
         try:
@@ -38,7 +38,39 @@ def wait_for_apds():
     return device
 
 apds = wait_for_apds()
+
+# turn on proximity detector
 apds.enable_proximity = True
 
-print("hello")
+# setup neopixel
+NEOPIXEL_PIN = 8
+# (yikes, this is very bright, make brightness less if it annoys you)
+pixel = neopixel.NeoPixel(board.NEOPIXEL, NEOPIXEL_PIN, brightness=1, auto_write=False)
+pixel.fill((255, 255, 255)) # start as white (quickly changes to show maximum distance which is red)
+pixel.show()
+print("Neopixel setup (white)")
 
+# Read and update neopixel
+print("Show proximity as hue...")
+while True:
+    # detects out to about 8cm for me.
+    # weird, my finger gives 0..250, but my whole hand only does 0..50
+    value = apds.proximity
+
+    if value > 8:
+        # values < 2 are boring to print
+        print( value )
+
+    # we can use 0..255 from the proximity as the hue
+    # so much easier in HSV
+    # (but notice that "close", i.e. 250, is purple (blue+red), try to fix this to only go to full-blue)
+    # More fun stuff with fancy:
+    #   https://learn.adafruit.com/fancyled-library-for-circuitpython/led-colors#secret-ingredient-gamma-correction-2981395-9
+    #   full docs at
+    #   https://circuitpython.readthedocs.io/projects/fancyled/en/latest/api.html
+    hsv_color = fancy.CHSV( value )
+
+    # neopixel only speaks rgb
+    rgb_color = fancy.CRGB( hsv_color )
+    pixel[0] = rgb_color.pack() # convert e.g. 0xFF1200, "int" style
+    pixel.show()
